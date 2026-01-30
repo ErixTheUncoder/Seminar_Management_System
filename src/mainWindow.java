092219f3c74bdc9a43f46a5239699af0e36e4453
ErixTheUncoder <erichong.0969@gmail.com>
2026-01-30 20:02:28 +0800

    change to using JPanel

--- src/mainWindow.java
+++ src/mainWindow.java
@@ -3,15 +3,21 @@ import javax.swing.*;
 
 public class mainWindow extends JFrame
 {
+
+  public mainWindow(){
+    JPanel mainPanel = new JPanel();
+    mainPanel.setBackground(Color.BLACK);
+
+    this.add(mainPanel);
+
+    super.setTitle("Hello World"); //Set Title
+    super.setSize(720,640); //Set the window size
+    super.setVisible(true); //Make the window visible
+    super.setResizable(false); //Make the window not resizable
+    super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Close the application instead of hiding
+  }
     public static void main(String[] args)
-    { 
-        JFrame frame = new JFrame(); //Create new Jframe
-        
-        frame.setTitle("First window");//Title
-        frame.setSize(720,640); //Set the window size
-        frame.setVisible(true); //Make the window visible
-        frame.setResizable(false); //Make the window not resizable
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Close the application instead of hiding
-        frame.getContentPane().setBackground(new Color(0,0,0));
+    {  //Create new Jframe
+        new mainWindow();
     }
 }
---
+++ src/tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+frame.setTitle("First window");//Title
